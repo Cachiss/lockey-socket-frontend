eb5c39f865215c14b6f232a5da6e91e48be58a06
Marcos Camacho <marcoscachis@gmail.com>
2023-04-04 17:28:47 -0600

    Add footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer class="bg-white rounded-lg shadow dark:bg-gray-800 h-20 flex flex-col items-center justify-center absolute right-0 left-0 bottom-0">
-        <div class="w-full mx-auto max-w-screen-xl  md:flex md:items-center md:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Lockey™</a>. Todos los derechos reservados.
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-800 h-20 flex flex-col items-center justify-center p-3">
+        <div className="w-full mx-auto max-w-screen-xl  md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Lockey™</a>. Todos los derechos reservados.
         </span>
         <div className='flex items-center '>
             <img width={"50px"} className='h-12' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="React" />
