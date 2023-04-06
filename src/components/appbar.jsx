f7d149167e2a4a031e20710f983871a4a557b4ca
Marcos Camacho <marcoscachis@gmail.com>
2023-04-05 21:23:07 -0600

    hide button login if is logged

--- src/components/appbar.jsx
+++ src/components/appbar.jsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import LockeyLogo from '../assets/lockey_logo.png'
 import GithubIcon from '../assets/github_icon.png'
+
 export const AppBar = ({onAboutClick = ()=>{}}) => {
+  const [loading, setLoading] = useState(true)
+  const [isLogged, setIsLogged] = useState(false)
+
+  useEffect(() => {
+    const logged = window.localStorage.getItem('logged')
+    if (logged) {
+      setIsLogged(true)
+    }
+    setLoading(false)
+  }, [])
+
+  if (loading) return <div className='bg-white border-gray-200 dark:bg-gray-900 w-screen fixed z-50'></div>
   return (
 
 <nav className="bg-white border-gray-200 dark:bg-gray-900 w-screen fixed z-50">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" className="flex items-center">
-        <img src={LockeyLogo} className="h-5 ml-3" alt="Flowbite Logo" />
+        <img src={LockeyLogo} className="h-5 ml-3" alt="Lockey Logo" />
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
@@ -26,8 +41,9 @@ export const AppBar = ({onAboutClick = ()=>{}}) => {
           <p className='className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"'>Sobre nosotros</p>
           </li>
 
-        <button className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 border border-transparent rounded-md hover:bg-blue-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">Iniciar sesión</button>
-
+        <Link to={"/login"}>
+        <button className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 border border-transparent rounded-md hover:bg-blue-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500" style={{"display": isLogged ? "none" : null}}>Iniciar sesión</button>
+        </Link>
       </ul>
     </div>
   </div>
